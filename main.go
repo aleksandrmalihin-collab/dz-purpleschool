851d384d64d8944b936ff442f0db6adc603c553b
Alexander <aleksandr.malihin@gmail.com>
2025-09-05 18:47:56 +0300

    Решение второго домашнего задания

--- main.go
+++ main.go
@@ -7,6 +7,19 @@ const usdTORub = 81.31
 const EurTOrub = usdTORub / usdTOEur
 
 func main() {
+	var num int
+	fmt.Scan(&num)
 	fmt.Println("=========Калькулятор расчеты валюты=========")
 	fmt.Println("1 Евро равен", EurTOrub, "рублей.")
+	stdin()
+	nullfunc(num, 20.1, 1000)
+}
+
+func stdin() {
+	var data int
+	fmt.Scan(&data)
+}
+
+func nullfunc(number int, cntuer float64, cntrub float64) {
+
 }
