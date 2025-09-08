1868fcef9fdc518f37a1f569144f184629dead14
Alexander <aleksandr.malihin@gmail.com>
2025-09-08 14:19:37 +0300

    Третье домашнее задание

--- main.go
+++ main.go
@@ -1,29 +1,71 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
-const usdTOEur = 0.85
-const usdTORub = 81.31
-const EurTOrub = usdTORub / usdTOEur
+const usdToRub = 79.1
+const usdToEur = 0.85
+const eurToRub = 85.1
+const eurToUsd = 1.17
+const rubToUsd = 0.012
+const rubToEur = 0.010
 
 func main() {
-	var num int
-	var valuta1, valuta2 string
-	fmt.Scan(&valuta1, &valuta2)
-	fmt.Scan(&num)
-	fmt.Println("=========Калькулятор расчеты валюты=========")
-	fmt.Println("1 Евро равен", EurTOrub, "рублей.")
-	a := stdin()
-	fmt.Println(a)
-	nullfunc(a, valuta1, valuta2)
+	sourceСurrency := validateCurrency()
+	amount := validateAmount()
+	targetCurrency := validateCurrency()
+	result := convertCurrency(sourceСurrency, amount, targetCurrency)
+	fmt.Printf("%.2f %s = %.2f %s\n", amount, sourceСurrency, result, targetCurrency)
 }
 
-func stdin() int {
-	var data int
-	fmt.Scan(&data)
-	return data
+func validateCurrency() string {
+	var currency string
+	fmt.Println("Доступны валюты: USD, RUB, EUR")
+	for {
+		fmt.Scan(&currency)
+		if currency != "USD" && currency != "RUB" && currency != "EUR" {
+			fmt.Println("Ошибка! Введите USD, EUR или RUB:")
+			continue
+		}
+		return currency
+	}
 }
 
-func nullfunc(amount int, fromCurrency string, toCurrency string) {
+func validateAmount() float64 {
+	var amount float64
+	for {
+		fmt.Println("Введите число")
+		_, err := fmt.Scan(&amount)
+		if err != nil {
+			fmt.Println("Ошибка! Введен неверный формат")
+			continue
+		}
+		if amount < 0 {
+			fmt.Println("Число не может быть меньше нуля")
+			continue
+		}
+		return amount
+	}
+}
 
+func convertCurrency(sourceСurrency string, amount float64, targetCurrency string) float64 {
+	var result float64
+	switch {
+	case sourceСurrency == "USD" && targetCurrency == "RUB":
+		result = amount * usdToRub
+	case sourceСurrency == "USD" && targetCurrency == "EUR":
+		result = amount * usdToEur
+	case sourceСurrency == "EUR" && targetCurrency == "USD":
+		result = amount * eurToUsd
+	case sourceСurrency == "EUR" && targetCurrency == "RUB":
+		result = amount * eurToRub
+	case sourceСurrency == "RUB" && targetCurrency == "EUR":
+		result = amount * rubToEur
+	case sourceСurrency == "RUB" && targetCurrency == "USD":
+		result = amount * rubToUsd
+	default:
+		result = amount
+	}
+	return result
 }
