1837c9be4a1d6a1938708c105a4a86a956101410
Alexander <aleksandr.malihin@gmail.com>
2025-09-09 14:02:15 +0300

    fix проекта номер 2

--- main.go
+++ main.go
@@ -8,7 +8,8 @@ import (
 
 func main() {
 	var input string
-	input = "10,20,30,40,50"
+	fmt.Println("Введите числа через запятую без пробелов!")
+	fmt.Scan(&input)
 	convertedInput := converterInputToInt(input)
 	myResult := operation(convertedInput)
 	fmt.Println("Мой итоговый результат:", myResult)
